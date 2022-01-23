55252e29cc73b3e8ff8d0bd313e9222baf7e9eec
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-22 20:22:20 -0600

    route folders/files + server.js file

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+/* API routes that should be created:
+GET /api/notes should read the db.json file * return all saved notes as JSON.
+
+POST /api/notes should receive a new note to save on the request body, 
+add it to the db.json file, and then return the new note to the client.
+-find a way to give each note a unique id when it's saved (look into npm 
+packages)
+*/
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,4 @@
+/* HTML routes that should be created:
+GET /notes should return the notes.html file.
+GET * should return the index.html file.
+*/
