76bd50cb5bd51192bbbc54cbbb733ec5a146937b
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-01-22 21:46:43 -0600

    api route updated

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,28 @@
-/* API routes that should be created:
-GET /api/notes should read the db.json file * return all saved notes as JSON.
+//import path and express/router packages
+const path = require('path');
+const router = require('express').Router();
 
-POST /api/notes should receive a new note to save on the request body, 
-add it to the db.json file, and then return the new note to the client.
--find a way to give each note a unique id when it's saved (look into npm 
-packages)
-*/
+//read db.json file
+const { notes } = require('../../db/db.json');
+
+// GET /api/notes -> 
+router.get('/api/notes', (req, res) => {
+    //return all saved notes as JSON
+    res.json(notes); //response turned into JSON and saved as "notes"
+})
+
+// POST /api/notes ->
+router.post('/api/notes', (req, res) => {
+
+    //give each note a unique id when it's saved
+    
+})
+// receive new note to save on req.body
+
+// add it to db.json file
+
+// return new note to the client
+
+
+
+
